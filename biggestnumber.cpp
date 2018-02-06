9762feffee985c1b76d1a8eaebfe0bfe3e3da8e0
CharcoalNuggets <dgin@mail.csuchico.edu>
2018-02-05 17:52:16 -0800

    Program decides what the bigger number is

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -5,13 +5,27 @@ using namespace std; //so we don't need the prefix std::
 
 int main()
 {
-  
+  int number1;
   cout<<"Please enter a whole number:\n";
-  
-  cout<<"Please enter another whole number:\n";
+  std::cin >> number1;
 
-  cout<<"Of those two numbers, the biggest is: ";
-  cout<<endl<<"Thank you for playing.\n";
+  int number2;
+  cout<<"Please enter another whole number:\n";
+  std::cin >> number2;
+  
+  int biggest;
+
+  if ( number1 > number2 )
+  { 
+  biggest = number1;
+  }
+  else
+  {
+  biggest = number2;
+  }
+
+ std::cout<<"Of those two numbers, the biggest is: " << biggest << "\n";
+ std::cout<<endl<<"Thank you for playing.\n";
 
   return 0;
 }
